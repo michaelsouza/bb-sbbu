6db52db7e7e61554a6122291c415822fe7ed1534
Michael Souza <souza.michael@gmail.com>
2022-12-07 09:46:10 -0300

    Remove disconnected graphs

--- codes/create_random.py
+++ codes/create_random.py
@@ -1,9 +1,10 @@
 import os
 import tqdm
 import numpy as np
+import networkx as nx
 from bb import order_brute, order_greedy, NMR
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     nnodes, nedges = 20, 5
     nsamples = 1000
     np.random.seed(1)
@@ -19,20 +20,25 @@ if __name__ == '__main__':
                 E[i].add(j)
                 lenE += 1
 
-        fn = 'DATA_TEST/testRAND_%04d.nmr' % k
-        with open(fn, 'w') as fid:
+        fn = "DATA_TEST/testRAND_%04d.nmr" % k
+        with open(fn, "w") as fid:
             for i in sorted(E):
                 for j in sorted(E[i]):
-                    fid.write('%d %d\n' % (i, j))
+                    fid.write("%d %d\n" % (i, j))
 
         nmr = NMR(fn)
         orderBF, costBF = order_brute(nmr)
         orderGD, costGD = order_greedy(nmr)
+        G = nmr.ordering_graph
 
-        if costBF != costGD:
-            MESSAGE.append('Found interesting instance (BF: %03d, GD: %03d, fn: %s).' % (
-                costBF, costGD, fn))
-        else:
+        if costBF == costGD:
             os.remove(fn)
+        elif nx.number_connected_components(G) > 1:
+            MESSAGE.append("Remove disconnected(fn: %s)." % fn)
+            os.remove(fn)
+        else:
+            msg = "Found interesting instance (BF: %03d, GD: %03d, fn: %s)."
+            MESSAGE.append(msg % (costBF, costGD, fn))
+
     for message in MESSAGE:
         print(message)
